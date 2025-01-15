b3b438cd492b4728a47e833e06e08cc118ec2ae4
Airon Tark <airon@tark.pro>
2025-01-15 10:53:33 +0200

    main.dart: rename it to tl_flutter_openai.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tl_flutter_openai/main.dart';
+import 'package:tl_flutter_openai/tl_flutter_openai.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
