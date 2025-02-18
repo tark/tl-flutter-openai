3c6420655bd378e26be3214ed152a38965d0ff90
Airon Tark <airon@tark.pro>
2025-02-18 09:36:14 +0200

    tl_flutter_openai.dart: add exports

--- lib/tl_flutter_openai.dart
+++ lib/tl_flutter_openai.dart
@@ -1,3 +1,7 @@
 library tl_flutter_openai;
 
 export 'src/tl_flutter_openai.dart';
+export 'src/models/models.dart';
+export 'src/models/models_new.dart';
+export 'src/models/chat_completion_response.dart';
+export 'src/models/message.dart';
